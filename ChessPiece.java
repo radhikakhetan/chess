f38baff05587b83f11591d0fda161eb8f8b9db3a
unknown <test@L-MAA-25004241.paypalcorp.com>
2015-07-29 12:15:26 +0530

    Add a class to represent a chess piece

---
+++ ChessPiece.java
@@ -0,0 +1,14 @@
+class ChessPiece {
+	
+	char name;
+	char color;
+	
+	public ChessPiece(char x, char y) {
+		name = x;
+		color = y;
+	}
+
+	public boolean isWhite() {
+		return color == 'W';
+	}
+}
