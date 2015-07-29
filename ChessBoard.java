cdd1f6d067933be990c696ba7ceb21a1d93766ad
radhika khetan <mnnit.radhika@gmail.com>
2015-07-29 14:17:50 +0530

    chessboard initialized

--- ChessBoard.java
+++ ChessBoard.java
@@ -1,11 +1,46 @@
 
-import java.util.Map;
+import java.util.*;
 
 public class ChessBoard {
 	
 	Map<String,ChessPiece> chessboard;
 	
 	ChessBoard(){
+		Map<String,ChessPiece> chessboard = new HashMap<String,ChessPiece>() ;
+		chessboard.put("a1", new ChessPiece('R','W')) ;
+		chessboard.put("b1", new ChessPiece('N','W')) ;
+		chessboard.put("c1", new ChessPiece('B','W')) ;
+		chessboard.put("d1", new ChessPiece('Q','W')) ;
+		chessboard.put("e1", new ChessPiece('K','W')) ;
+		chessboard.put("f1", new ChessPiece('B','W')) ;
+		chessboard.put("g1", new ChessPiece('N','W')) ;
+		chessboard.put("h1", new ChessPiece('R','W')) ;
+		chessboard.put("a2", new ChessPiece(' ','W')) ;
+		chessboard.put("b2", new ChessPiece(' ','W')) ;
+		chessboard.put("c2", new ChessPiece(' ','W')) ;
+		chessboard.put("d2", new ChessPiece(' ','W')) ;
+		chessboard.put("e2", new ChessPiece(' ','W')) ;
+		chessboard.put("f2", new ChessPiece(' ','W')) ;
+		chessboard.put("g2", new ChessPiece(' ','W')) ;
+		chessboard.put("h2", new ChessPiece(' ','W')) ;
+		chessboard.put("a7", new ChessPiece(' ','B')) ;
+		chessboard.put("b7", new ChessPiece(' ','B')) ;
+		chessboard.put("c7", new ChessPiece(' ','B')) ;
+		chessboard.put("d7", new ChessPiece(' ','B')) ;
+		chessboard.put("e7", new ChessPiece(' ','B')) ;
+		chessboard.put("f7", new ChessPiece(' ','B')) ;
+		chessboard.put("g7", new ChessPiece(' ','B')) ;
+		chessboard.put("h7", new ChessPiece(' ','B')) ;
+		chessboard.put("a8", new ChessPiece('R','B')) ;
+		chessboard.put("b8", new ChessPiece('N','B')) ;
+		chessboard.put("c8", new ChessPiece('B','B')) ;
+		chessboard.put("d8", new ChessPiece('Q','B')) ;
+		chessboard.put("e8", new ChessPiece('K','B')) ;
+		chessboard.put("f8", new ChessPiece('B','B')) ;
+		chessboard.put("g8", new ChessPiece('N','B')) ;
+		chessboard.put("h8", new ChessPiece('R','B')) ;
+		
+		
 		
 	}
 	char findPiece(String move){
