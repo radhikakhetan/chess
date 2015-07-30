4d93f2d4aaede6926460888394572bd0f9e087b5
Aravind Srivatsan <hari.srivats@gmail.com>
2015-07-30 11:07:53 +0530

    Added useDelimter function to read pgn files

--- Chess.java
+++ Chess.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class Chess {
 	public static void main (String [] args) throws FileNotFoundException {
 		ChessBoard chessboard = new ChessBoard() ;
-		if (args.length == 0) {
+		/*if (args.length == 0) {
 			System.out.println("Usage java Chess <filename>");
 			return;
-		}
-		Scanner reader = new Scanner(new File(args[0]));
+		}*/
+		Scanner reader = new Scanner(new File("C:\\Users\\test\\workspace\\chess\\src\\input.txt")).useDelimiter(" *\\d[.] *");
 		while ( reader.hasNextLine() ) {
-			String[] round = reader.nextLine().trim().split(" ");
+			String[] round = reader.next().trim().split(" ");
 			chessboard.move(round[0], 1);
-			chessboard.move(round[1], 2);
+			if( round.length > 1)
+				chessboard.move(round[1], 2);
 		}
 		reader.close();
 		chessboard.displayBoard();
