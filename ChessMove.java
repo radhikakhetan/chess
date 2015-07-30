c5adccdd3551b423127389bc8fc0ddb4b05ebad6
Bhavana Jain <jain.bhavana1@gmail.com>
2015-07-30 09:52:58 +0530

    variable to check for E.P. added

--- ChessMove.java
+++ ChessMove.java
@@ -1,3 +1,5 @@
+package chess;
+
 public class ChessMove {
 
 	boolean isCapturing;
@@ -6,8 +8,9 @@ public class ChessMove {
 	char ambiguity;
 	boolean kingCastling;
 	boolean queenCastling;
+	boolean isEnpassant;
 	ChessPiece cp;
-	
+
 	ChessMove(String move, int turn) {
 		queenCastling = false;
 		kingCastling = false;
@@ -15,72 +18,93 @@ public class ChessMove {
 		file = ' ';
 		rank = -1;
 		isCapturing = false;
-		
-		if(move.equals("O-O")){
+		isEnpassant = false;
+
+		if (move.equals("O-O")) {
 			kingCastling = true;
 			return;
-		}
-		else if(move.equals("O-O-O")){
+		} else if (move.equals("O-O-O")) {
 			queenCastling = true;
 			return;
 		}
-		
-		char lastChar = move.charAt(move.length()-1);
-		if (lastChar == '+' || lastChar == '#'){
+
+		char lastChar = move.charAt(move.length() - 1);
+		if (lastChar == '+' || lastChar == '#') {
 			move = removeLastChar(move);
+		} else if (move.contains("e.p."))
+			;
+		{
+			isEnpassant = true;
+			int i = 0;
+			while (i < 4) {
+				move = removeLastChar(move);
+				i++;
+			}
 		}
 		rank = Character.getNumericValue((move.charAt(move.length() - 1)));
 		file = move.charAt(move.length() - 2);
-		
-		if(move.contains("x")){
+
+		if (move.contains("x")) {
 			isCapturing = true;
 		}
-		
-		if(move.length() > 3){
+
+		if (move.length() > 3 && move.charAt(0) != 'x') {
 			ambiguity = move.charAt(1);
 		}
 
 		char name = ' ';
-		if(Character.isUpperCase(move.charAt(0))){
+		if (Character.isUpperCase(move.charAt(0))) {
 			name = move.charAt(0);
 		}
-		
+
 		char color = 'B';
-		if(turn == 1) {
+		if (turn == 1) {
 			color = 'W';
 		}
-		cp = new ChessPiece(name,color);
-		
+		cp = new ChessPiece(name, color);
 	}
-	ChessPiece getChessPiece(){
+
+	ChessPiece getChessPiece() {
 		return cp;
 	}
+
 	private String removeLastChar(String str) {
-        return str.substring(0,str.length()-1);
-    }
-	boolean isCapturing(){
+		return str.substring(0, str.length() - 1);
+	}
+
+	boolean isCapturing() {
 		return isCapturing;
 	}
-	boolean isAmbiguous(){
-		if(ambiguity == ' ')
+
+	boolean isAmbiguous() {
+		if (ambiguity == ' ')
 			return false;
 		else
 			return true;
 	}
-	char getAmbiguousPosition(){
+
+	char getAmbiguousPosition() {
 		return ambiguity;
 	}
-	boolean isCastling(){
+
+	boolean isCastling() {
 		return kingCastling || queenCastling;
 	}
-	boolean isKingCastling(){
+
+	boolean isKingCastling() {
 		return kingCastling;
 	}
-	boolean isQueenCastling(){
+
+	boolean isQueenCastling() {
 		return queenCastling;
 	}
-    String getMove(){
-    	return file+""+rank;
-    }
-	
+
+	boolean isEnpassant() {
+		return isEnpassant;
+	}
+
+	String getMove() {
+		return file + "" + rank;
+	}
+
 }
