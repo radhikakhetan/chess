89d030e35bde84efe93482e16ea30cdd113dfc61
Aravind Srivatsan <hari.srivats@gmail.com>
2015-07-30 11:32:50 +0530

    Renaming base class

---
+++ PGNReader.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PGNReader {
+	public static void main (String [] args) throws FileNotFoundException {
+		ChessBoard chessboard = new ChessBoard() ;
+		/*if (args.length == 0) {
+			System.out.println("Usage java Chess <filename>");
+			return;
+		}*/
+		Scanner reader = new Scanner(new File("C:\\Users\\test\\workspace\\chess\\src\\input.txt")).useDelimiter(" *\\d[.] *");
+		while ( reader.hasNextLine() ) {
+			String[] round = reader.next().trim().split(" ");
+			chessboard.move(round[0], 1);
+			if( round.length > 1)
+				chessboard.move(round[1], 2);
+		}
+		reader.close();
+		chessboard.displayBoard();
+	}
+
+}
